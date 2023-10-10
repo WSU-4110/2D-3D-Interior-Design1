3f3db8e3f2b523f19f0162a24e2b317b9c891dd0
Lavender C <lmcalhoun@wayne.edu>
2023-10-10 16:39:11 -0400

    adding slider, menu, undo and redo, bugs present

--- RoomRaiser.py
+++ RoomRaiser.py
@@ -1,5 +1,8 @@
 import tkinter as tk
 from tkinter.colorchooser import askcolor
+from tkinter import ttk, Menu, filedialog, messagebox
+from PIL import Image, ImageDraw
+import itertools
 import math
 
 
@@ -18,20 +21,43 @@ class GridDrawingApp(tk.Tk):
         self.toolbar.pack(side=tk.TOP, fill=tk.X)
         
         self.itembar = tk.Frame(self)
-        self.itembar.pack(side=tk.RIGHT, fill = tk.Y)
-        
+        self.itembar.pack(side=tk.TOP, fill= tk.Y)
 
         # Add buttons to the toolbar
         self.clear_button = tk.Button(self.toolbar, text="Clear Canvas", command=self.clear_canvas)
         self.clear_button.pack(side=tk.LEFT)
 
         # Default line color
+        DEFAULT_COLOR = 'black'
         self.line_color = "black"
+        self.pen_size_num = 2
         
+        #Pen size slider
+        self.pen_size = tk.Scale(self.toolbar, from_=1, to=10, orient="horizontal", label="Pen Size", command=self.update_pen_size)
+        self.pen_size.pack(side=tk.LEFT)
+        
+        self.undo_button = tk.Button(self.toolbar, text="Undo", command=self.undo)
+        self.undo_button.pack(side=tk.LEFT)
+        self.redo_button = tk.Button(self.toolbar, text="Redo", command=self.redo)
+        self.redo_button.pack(side=tk.LEFT)
+        
+        #Menu Bar
+        menubar = Menu(self.master)
+        self.config(menu=menubar)
+        
+        
+        fileMenu = Menu(menubar)
+        fileMenu.add_command(label="Export", command = self.exportToImage)
+        fileMenu.add_command(label="Exit", command=self.onExit)
+        menubar.add_cascade(label="File", menu=fileMenu)
         
         # Add a label to display line info
         self.line_info_label = tk.Label(self.toolbar, text="Line Length: 0.0, Angle: 0.0 degrees")
         self.line_info_label.pack(side=tk.RIGHT)
+        
+        # undo and redo
+        
+        self.line_history = []
 
         # Bind the canvas to the window resizing event
         self.canvas.bind("<Configure>", self.create_grid)
@@ -72,7 +98,7 @@ class GridDrawingApp(tk.Tk):
 
         # Create a line starting from the snapped point
         self.canvas.data = [x, y]
-        self.canvas.line = self.canvas.create_line(x, y, x, y, fill="black", width=3)
+        self.canvas.line = self.canvas.create_line(x, y, x, y, fill=self.line_color, width=self.pen_size_num)
 
 
     def update_line(self, event):
@@ -96,15 +122,13 @@ class GridDrawingApp(tk.Tk):
             x, y = self.snap_to_grid(event.x, event.y)
             
             # Add the drawn line to the history
-            line = self.canvas.create_line(self.canvas.data[0], self.canvas.data[1], x, y, fill = self.line_color, width=3)
-            self.line_history.append(line)
-
-            # Clear the redo history
-            self.redo_history.clear()
-
-            # Delete any existing line on the canvas
-            self.canvas.delete(self.canvas.line)
             
+            
+            self.line_history.append((self.canvas.data[0], self.canvas.data[1], x, y, self.line_color, self.pen_size))
+            new_line = self.canvas.create_line(self.canvas.data[0], self.canvas.data[1], x, y, self.line_color, self.pen_size)
+            self.canvas.delete(new_line)
+
+
             # Calculate and display line length and angle
             length = math.sqrt((x - self.canvas.data[0])**2 + (y - self.canvas.data[1])**2)
             angle = math.degrees(math.atan2(self.canvas.data[1]-y, x-self.canvas.data[0]))
@@ -114,11 +138,60 @@ class GridDrawingApp(tk.Tk):
             # Update the line info label
             self.line_info_label.config(text=f"Line Length: {length/20:.2f}, Angle: {angle:.2f} degrees")
             
-            del self.canvas.data
             
     def clear_canvas(self):
         self.canvas.delete("all")
         self.create_grid()
+        
+    def update_pen_size(self, value):
+        self.pen_size_num = int(value)
+        
+    def undo(self):
+        if self.line_history:
+            last_line = self.line_history.pop()
+            self.canvas.delete(last_line)
+
+    def redo(self):
+        if self.redo_history:
+            self.line_history.append(self.redo_history.pop())
+            self.redraw_lines()
+    
+    def redraw_lines(self):
+        # Clear the canvas
+        self.canvas.delete("all")
+        self.create_grid()
+
+        # Redraw the lines from history
+        for line_data in self.line_history:
+            x1, y1, x2, y2, color, pen_size = line_data
+            self.canvas.create_line(x1, y1, x2, y2, fill=color, width=pen_size)
+        
+    # Menu Items
+    
+    def exportToImage(self):
+
+           file_path = filedialog.asksaveasfilename(defaultextension=".png", filetypes=[("PNG files", "*.png"), ("All files", "*.*")])
+           
+           if file_path:
+                # Create an empty PIL image with the same size as the canvas
+                image = Image.new("RGBA", (self.canvas.winfo_width(), self.canvas.winfo_height()), (255, 255, 255, 0))
+
+                # Create a PIL Draw object to draw on the image
+                draw = ImageDraw.Draw(image)
+
+                # Redraw the lines from history on the image
+                for line_data in self.line_history:
+                    x1, y1, x2, y2, color, pen_size = line_data
+                    draw.line([(x1, y1), (x2, y2)], fill=color, width=pen_size)
+
+                # Save the image as a PNG file
+                image.save(file_path, "PNG")
+
+
+        
+                
+    def onExit(self):
+        self.quit()
         
 
 if __name__ == "__main__":
