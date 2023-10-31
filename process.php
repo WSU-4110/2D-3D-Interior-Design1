6a20aad98d782ba397f3e42782c9b452140f28b3
humaira1526 <105958595+humaira1526@users.noreply.github.com>
2023-10-31 13:07:25 -0400

    Update process.php

--- process.php
+++ process.php
@@ -2,27 +2,3 @@
 $con = mysqli_connect("localhost", "root","","register") or die(mysqli_error());
 
 ?>
-
-
-
-
-
-<?php
-    session_start();
-    include("process.php");
-
-    if($_SERVER['RREQUEST_METHOD'] == "POST"){
-        $email = $_POST['email'];
-        $password = $_POST['password'];
-
-        if(!empty($email) && !empty($password) && !is_numeric($email)){
-            $query = "INSERT INTO users (email, password) VALUES('$email', '$password')";
-            mysqli_query($con, $query);
-            echo "<script type='text/javascript'> alert('Successfully registered')</script>"
-        }
-        else{
-            echo "<script type='text/javascript'> alert('couldn't register')</script>"
-
-        }
-    }
-?>
