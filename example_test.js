4629e7ac74d6acca1bb23cd38d0b29c18b3043db
Naz1380 <136735350+Naz1380@users.noreply.github.com>
2023-11-17 21:33:55 -0500

    Create example_test.js
    add mocha test file

---
+++ example_test.js
@@ -0,0 +1,8 @@
+// test/example-test.js
+const assert = require('assert');
+
+describe('Example Test', function () {
+  it('should pass when true is true', function () {
+    assert.strictEqual(true, true);
+  });
+});
