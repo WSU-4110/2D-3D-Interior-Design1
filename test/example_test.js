c84da5362b5bb9d968793560f1fe8ea6da32faaf
Naz1380 <136735350+Naz1380@users.noreply.github.com>
2023-11-18 14:34:21 -0500

    Update and rename example_test.js to test/example_test.js

--- test/example_test.js
+++ test/example_test.js
@@ -1,7 +1,8 @@
 // example_test.js
 
 const assert = require('assert');
-const myModule = require();
+const myModule = require('../path/to/2D-3D-Interior-Design1/platform_Naz');
+
 
 describe('MyModule', () => {
   it('should do something', () => {
